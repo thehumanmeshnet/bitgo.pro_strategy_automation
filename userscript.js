2e7057d94cb3e71c88d7d491b288581a3b5838c5
Anon Y Mous <thehumanmeshnet@users.noreply.github.com>
2017-08-23 23:52:32 +0530

    Update userscript.js
    added autowithdraw option

--- userscript.js
+++ userscript.js
@@ -1,5 +1,3 @@
-// use this in greasemonkey on chrome
-
 // ==UserScript==
 // @name         New Userscript
 // @namespace    http://tampermonkey.net/
@@ -13,7 +11,47 @@
 (function() {
     'use strict';
     setTimeout(function(){
-        if(document.querySelector('body > div > section.header > div.header-middle > div.container > div > div:nth-child(2) > div:nth-child(4) > a.btn.btn-1').innerHTML.split('Login').length === 2 && window.location.href !== 'https://bitgopro.biz/index.php?a=login'){
+        var a = new Date();
+        var hours = a.getHours();
+        var mins = a.getMinutes();
+        if(!(hours === 13 && mins > 35)){
+            localStorage.done = true;
+        }
+        if(window.location.href === 'https://bitgopro.biz/index.php?a=withdraw'){
+            if(localStorage.daily_withdrawal > 0 && hours === 13 && mins >35){
+                var account_balance = document.querySelector('body > div > section.main.main-cabinet > div > div > form > table:nth-child(6) > tbody > tr:nth-child(1) > td:nth-child(2) > b').innerHTML;
+                account_balance = parseFloat(account_balance);
+                var limit = localStorage.daily_withdrawal.toString().substr(0,4);
+                var Nlimit = parseFloat(limit);
+                if(Nlimit < account_balance){
+                    document.querySelector('body > div > section.main.main-cabinet > div > div > form > table:nth-child(8) > tbody > tr:nth-child(5) > td:nth-child(1) > input[type="radio"]').click();
+                    document.querySelector('body > div > section.main.main-cabinet > div > div > form > table:nth-child(9) > tbody > tr:nth-child(2) > td:nth-child(2) > input').value = limit;
+                    document.querySelector('body > div > section.main.main-cabinet > div > div > form > table:nth-child(9) > tbody > tr:nth-child(4) > td:nth-child(2) > input').click();
+                    localStorage.done = 'done';
+                    window.location.href = 'https://bitgopro.biz/index.php?a=deposit';
+                    return;
+                }
+                else {
+                    window.location.reload();
+                    return;
+                }
+            }
+        }
+        if(localStorage.done !== 'done' && window.location.href === 'https://bitgopro.biz/index.php?a=account'){
+            var current_investment = document.querySelector('body > div > section.main.main-cabinet > div > div > div:nth-child(3) > table > tbody > tr:nth-child(6) > td:nth-child(2) > b').innerHTML;
+            current_investment = parseFloat(current_investment);
+            localStorage.daily_withdrawal = current_investment*0.1;
+            window.location.href = 'https://bitgopro.biz/index.php?a=withdraw';
+            return;
+        }
+        if( ( localStorage.done !== 'done' && (hours === 13 && mins > 35)) && document.querySelector('body > div > section.header > div.header-middle > div.container > div > div:nth-child(2) > div:nth-child(4) > a.btn.btn-1').innerHTML.split('Login').length === 1){
+            localStorage.done = false;
+            window.location.href = 'https://bitgopro.biz/index.php?a=account';
+            return;
+        }
+        
+        var loggedIn = document.querySelector('body > div > section.header > div.header-middle > div.container > div > div:nth-child(2) > div:nth-child(4) > a.btn.btn-1').innerHTML.split('Login').length === 2;
+        if(loggedIn && window.location.href !== 'https://bitgopro.biz/index.php?a=login'){
             window.location.href = 'https://bitgopro.biz/index.php?a=login';
             return;
         }
